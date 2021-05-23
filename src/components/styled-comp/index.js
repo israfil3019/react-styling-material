a6e5bbf2d8bc9bbaf993746e8250b23cbe7d4683
israfil <israfil3019@gmail.com>
2021-05-23 18:53:19 +0300

    add link, extended link and onClick to button

--- src/components/styled-comp/index.js
+++ src/components/styled-comp/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Button from "./button/Button";
+import ExtendedLearnFSD from "./extended-link/ExtendedLearnFSD";
 import HeaderText from "./header/HeaderText";
+import LearnFSD from "./link/LearnFSD";
 import Wrapper from "./wrapper/Wrapper";
 
 const StyledComponents = () => {
+  const [primary, setPrimary] = React.useState(false);
   return (
     <Wrapper bgColor="wheat">
       <HeaderText>My Way</HeaderText>
       <HeaderText headerColor="purple">Styled Components</HeaderText>
-      <Button primary>Primary</Button>
-      <Button>Default</Button>
+      <LearnFSD secondary href="https://google.com" target="_blank">
+        Learn Full Stack Development
+      </LearnFSD>
+      <ExtendedLearnFSD>We'are Learning Styled Components</ExtendedLearnFSD>
+      <Button primary={primary} onClick={() => setPrimary(!primary)}>
+        {primary ? "Primary" : "Default"}
+      </Button>
+      <Button primary={!primary}>{!primary ? "Primary" : "Default"}</Button>
     </Wrapper>
   );
 };
