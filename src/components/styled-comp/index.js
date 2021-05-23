2ce58429cd80f6b00cc51edecbf2720e1dbf22a1
israfil <israfil3019@gmail.com>
2021-05-23 17:35:15 +0300

    add header

--- src/components/styled-comp/index.js
+++ src/components/styled-comp/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Button from "./button/Button";
+import HeaderText from "./header/HeaderText";
 import Wrapper from "./wrapper/Wrapper";
 
 const StyledComponents = () => {
   return (
     <Wrapper bgColor="wheat">
+      <HeaderText>My Way</HeaderText>
+      <HeaderText headerColor="purple">Styled Components</HeaderText>
       <Button primary>Primary</Button>
       <Button>Default</Button>
     </Wrapper>
