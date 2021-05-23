79df73b93ebbfed731556f51b0a073e3bf0aa47e
israfil <israfil3019@gmail.com>
2021-05-23 21:57:59 +0300

    add Typography and theme

--- src/App.js
+++ src/App.js
@@ -3,19 +3,45 @@ import Buttons from "./components/material-ui/buttons/Buttons";
 import CheckBoxComp from "./components/material-ui/checkbox/CheckBoxComp";
 import StyledButton from "./components/material-ui/styled-button/StyledButton";
 import TextFieldComp from "./components/material-ui/text-field/TextFieldComp";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { orange, green } from "@material-ui/core/colors";
+import TypographyComp from "./components/material-ui/typography/TypographyComp";
 // import StyledComponents from "./components/styled-comp/index";
+
+const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontSize: 38,
+      marginBottom: 5,
+    },
+    subtitle1: {
+      marginBottom: 10,
+    },
+  },
+  palette: {
+    primary: {
+      main: orange[900],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <div className="Header">
-        {/* <StyledComponents /> */}
-        <StyledButton />
-        <TextFieldComp />
-        <CheckBoxComp />
-        <Buttons />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <div className="Header">
+          {/* <StyledComponents /> */}
+          <TypographyComp />
+          <StyledButton />
+          <TextFieldComp />
+          <CheckBoxComp />
+          <Buttons />
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/material-ui/typography/TypographyComp.js
@@ -0,0 +1,17 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+const TypographyComp = () => {
+  return (
+    <>
+      <Typography variant="h2" component="div">
+        My Way MUI Session
+      </Typography>
+      <Typography variant="subtitle1" component="div">
+        We're learning Material UI
+      </Typography>
+    </>
+  );
+};
+
+export default TypographyComp;
